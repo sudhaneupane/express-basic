00833a16d4e669a4ac6f40f9f504c99766608429
Sudha <sudaneupane.11@gmail.com>
2024-04-02 12:31:59 +0545

    patient Schema

--- hospital-management/patient.models.js
+++ hospital-management/patient.models.js
@@ -1,5 +1,36 @@
 import mongoose from "mongoose"
 
-const patientSchema=new mongoose.Schema({},{timestamps:true})
+const patientSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    diagnosedWith:{
+        type:String,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true,
+    },
+    age:{
+        type:Number,
+        required:true,
+    },
+    bloodGroup:{
+        type:String,
+        required:true,
+    },
+    gender:{
+        type:String,
+        required:true,
+        enum:["MALE","FEMALE","OTHERS"]
+    },
+    admittedIn:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Hospital",
+        required:true,
+    },
+},{timestamps:true})
 
 export const Patient=mongoose.model("Patient",patientSchema)
